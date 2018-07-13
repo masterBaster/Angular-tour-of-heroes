b9ba8e0aace0f7e7fc062f9fe2f458c77dd898d0
masterBaster <bartek.baster@gmail.com>
2018-07-13 17:42:55 +0200

    Display the message from HeroService

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -10,8 +11,9 @@ import { Observable, of } from 'rxjs';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroServie: fetched heroes')
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
