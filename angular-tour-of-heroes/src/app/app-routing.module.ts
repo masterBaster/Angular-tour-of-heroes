d3c94a49f994451ca97e0a6c829f8a4440e7c0ad
masterBaster <bartek.baster@gmail.com>
2018-07-17 22:00:05 +0200

    added dashboard component

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes:Routes = [
   {path: 'home', children:[
-      { path: 'heroes', component: HeroesComponent }]
+      { path: 'heroes', component: HeroesComponent },
+      {path: 'dashboard', component: DashboardComponent}]
   }
 ];
 
