1c06e4990cd9d3daa570d030b08ab3537cc38b71
masterBaster <bartek.baster@gmail.com>
2018-07-22 12:02:39 +0200

    working with http service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -13,8 +13,12 @@ import { catchError, map, tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class HeroService {
-
-  private heroesUrl = 'api/heroes';  
+  
+  private heroesUrl = 'api/heroes'; 
+  
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
@@ -27,21 +31,17 @@ export class HeroService {
     };
   }
 
-  getHeroes (): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroServie: fetched heroes')
     return this.http.get<Hero[]>(this.heroesUrl)
-      .pipe(
-        tap(heroes => this.log('fetched heroes')),
-        catchError(this.handleError('getHeroes', []))
-      );
+    .pipe(
+      tap(heroes => this.log('fetched heroes')),
+      catchError(this.handleError('getHeroes', [])))
   }
 
   getHero(id: number): Observable<Hero>{
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
-  }
-
-  private log(message: string){
-    this.messageService.add(`HeroService: ${message}`);
   }
 
   constructor(
